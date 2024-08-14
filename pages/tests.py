a347485883494697feef1f4970ce84671e7b8749
Caner Ekinci <canereknc37@gmail.com>
2024-08-14 12:07:52 +0300

    Pages app tests are added

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from .views import home
+
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "Sorwe App Home")
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Random App Page")
+
+    def test_homepage_view(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, home.__name__)
